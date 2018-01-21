de287811ec432cfa541f5ca54b5a379f343c2460
vladkrylov <vladkrilov9@gmail.com>
2018-01-21 18:26:43 +0200

    forms

--- app/views.py
+++ app/views.py
@@ -1,16 +1,30 @@
 from app import app
-from flask import render_template, make_response
+from flask import render_template, make_response, request
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/test_response')
+
+@app.route('/test_response', methods=('GET', 'POST'))
 def test_response():
     text = '''
-<form action="https://google.com/search">
-  <input name="q">
+<form action="post">
+  What is your birthday?
+  <br>
+  <label> Month
+  <input type="text" name="month">
+  </label>
+  <label> Day
+  <input type="text" name="day">
+  </label>
+  <label> Year
+  <input type="text" name="year">
+  </label>
+  <br>
+  <br>
   <input type="submit">
 </form>
     '''
@@ -19,3 +33,15 @@ def test_response():
     return make_response((text,
                           200,
                           headers))
+
+
+@app.route('/testform', methods=('GET', 'POST'))
+def testform():
+    headers = {}
+    headers['Content-Type'] = 'text/plain'
+    q = request.args.get('q')
+    print(str(request))
+    return make_response((request.get_data(),
+                          200,
+                          headers))
+
